c77e81f52ee7334a5092058aeb331494ba9e85a4
Hemanth Kumar M S <Hemanthhp1229@gmail.com>
2021-04-09 08:07:24 +0530

    Completed Boss Challenge

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+//        print(sender.currentTitle as Any)
+        playSound(soundName: sender.currentTitle ?? "C")
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+       //This code brings sender's opacity back to fully opaque.
+        /* for smooth transition*/    UIView.animate(withDuration: 0.3) {
+                        sender.alpha = 1.0
+                    }
+      }
+    }
     
-
+    func playSound(soundName:String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        print(url!)
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                	
+    }
 }
-
